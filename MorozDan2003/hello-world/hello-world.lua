584ac7906247eabda507ee9c1f46c35f29f5832b
MorozDan2003 <morozdan2003@gmail.com>
2019-08-07 22:54:14 +0300

    Making example program

---
+++ MorozDan2003/hello-world/hello-world.lua
@@ -0,0 +1,5 @@
+for i = 1, 5 do
+	computer.beep()
+	end
+
+print('Hello World!')
--- Vexatos/example.lua
+++
@@ -1,7 +0,0 @@
---[[This is an example for how to correctly place OC programs into this repository.
-  As you can see, this file is inside a folder called 'Vexatos', which is named after the person
-  who placed it in here.]]
-
-for i=1,10 do
-  print("Hello World "..i.."!")
-end
